9b6b0f2b29a85c224b319148bf97a1ee03864808
muhammed-saeed <mohammed.yahia3@gmail.com>
2022-10-09 20:42:56 +0200

    file

--- nubian.py
+++ nubian.py
@@ -1,2 +1,15 @@
 import fitz
 print(fitz.__doc__)
+
+pdf_file = "/home/CE/musaeed/Old Nubian Texts from Gebel.pdf"
+doc = fitz.open(pdf_file)
+
+toc = doc.get_toc()
+print(f"the toc {toc}")
+
+page_number = 10
+page = doc[page_number]  # the short form
+print(f"the page number {page_number} is {page}")
+text = page.get_text("block")
+print(f"the text is {text}")
+
